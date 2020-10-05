8afd5c1d496182eddefa4fe96c539519dfe8622b
masa <masatoshi.sboard@gmail.com>
2020-10-05 21:19:01 +0900

    Done b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -4,5 +4,19 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        k: usize,
+        h: [isize; n],
     }
+    let mut dp = vec![0; n];
+    for i in 1..n {
+        let min = std::cmp::max(0, i as i64 - k as i64) as usize;
+        dp[i] = dp[min..i]
+            .iter()
+            .enumerate()
+            .map(|(j, &c)| c + (h[i] - h[j + min]).abs())
+            .min()
+            .unwrap();
+    }
+    println!("{}", dp[n - 1]);
 }
