f1626d723bd13ebe15cd08ab804044aabee4ed63
masa <masatoshi.sboard@gmail.com>
2020-10-05 23:01:44 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,28 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        activities: [[usize; 3]; n],
     }
+    let mut dp = vec![vec![0; 3]; n + 1];
+
+    for i in 0..n {
+        for j in 0..3 {
+            for k in 0..3 {
+                if j == k {
+                    continue;
+                }
+                if dp[i + 1][k] < dp[i][j] + activities[i][k] {
+                    dp[i + 1][k] = dp[i][j] + activities[i][k];
+                }
+            }
+        }
+    }
+    let mut max = dp[n][0];
+    for i in 1..3 {
+        if max < dp[n][i] {
+            max = dp[n][i];
+        }
+    }
+    println!("{}", max);
 }
