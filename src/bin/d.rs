1b436b50bed15f11f4502c16a642250fdaa0d2d3
masa <masatoshi.sboard@gmail.com>
2020-10-15 23:24:08 +0900

    Done d

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -4,5 +4,22 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        w: usize,
+        g: [(usize, usize); n],
     }
+    let mut dp = vec![vec![0; 100100]; 110];
+    for i in 0..n {
+        for sum_w in 0..=w {
+            if sum_w >= g[i].0 {
+                if dp[i + 1][sum_w] < dp[i][sum_w - g[i].0] + g[i].1 {
+                    dp[i + 1][sum_w] = dp[i][sum_w - g[i].0] + g[i].1;
+                }
+            }
+            if dp[i + 1][sum_w] < dp[i][sum_w] {
+                dp[i + 1][sum_w] = dp[i][sum_w];
+            }
+        }
+    }
+    println!("{}", dp[n][w]);
 }
